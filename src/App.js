0cba5a3377b181552b534193399ad49957e5cfc6
Fedenko Maksym <feden2906@gmail.com>
2020-12-09 17:14:59 +0200

    no message

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React, {Component} from 'react';
 import './App.css'
 import Header from "./components/header/Header";
+import {UserServices} from "./components/services/UserServices";
+import {PostServices} from "./components/services/PostServices";
 
+import {
+  BrowserRouter as Router, Switch, Route, NavLink
+} from "react-router-dom";
+
+import AllUsers from "./components/allUsers/AllUsers";
+import AllPosts from "./components/allPosts/AllPosts";
 
 class App extends Component {
 
-  // state = {inputValue: ''}
-  // setTitle = (value) => {
-  //   this.setState({inputValue: value})
-  // }
+  state = {
+    users: [], usersFlag: '',
+    posts: [], postsFlag: ''
+  }
+
+  UserServices = new UserServices()
+  PostServices = new PostServices()
+
+  async componentDidMount() {
+    let users = await this.UserServices.getAllUsers()
+    let posts = await this.PostServices.getAllPosts()
+    this.setState({
+      users, usersFlag: 'loaded',
+      posts, postsFlag: 'loaded'
+    })
+  }
 
   render() {
+    const {users, usersFlag, posts, postsFlag} = this.state
     return (
         <div>
-          <Header/>
-          {/*{<input type="title" onChange={e => this.setTitle(e.target.value)}/>}*/}
+          <Header Router={Router} NavLink={NavLink}/>
+          <Router>
+            <Switch>
+              <Route path={'/Користувачі'} render={() => <AllUsers users={users} flag={usersFlag}/>}/>
+              <Route path={'/Пости'} render={() => <AllPosts posts={posts} flag={postsFlag}/>}/>
+              {/*<Route path={'/Фото'} component={}/>*/}
+              {/*<Route path={'/Коменти'} component={}/>*/}
+            </Switch>
+          </Router>
         </div>
     );
   }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,15 +2,15 @@ import React, {Component} from 'react';
 import './Header.css'
 import NavItem from "../navItem/NavItem";
 
-
 class Header extends Component {
 
   nav = ['Користувачі', 'Пости', 'Фото', 'Коменти']
 
   render() {
+    const {...props} = this.props
     return (
         <div className='nav'>
-          {this.nav.map(value => <NavItem item={value} key={value}/>)}
+          {this.nav.map(value => <NavItem item={value} key={value} props={props}/>)}
         </div>
     );
   }
